6f9e2791790671f60f37746753104b9e78d9da54
ErofeevMA-Git <scorpxmakc@gmail.com>
2025-12-02 17:50:01 +0000

    02.12.25

---
+++ Linux/bash/FinalProject/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_dir=$(dirname "$(readlink -f "$0")")
+OUTPUT_FILE="$script_dir/OUTPUT.txt"
+> $OUTPUT_FILE
+
+for dir in /proc/[0-9]*
+do
+    if [ -d $dir ] 
+        then
+            echo $(basename "$dir") >> $OUTPUT_FILE
+    else
+        echo "Error"
+    fi
+done

74f43834caceae4dcbaa8dda084581881163c1c4
ErofeevMA-Git <scorpxmakc@gmail.com>
2025-12-10 17:12:43 +0000

    10/12/25

--- Linux/bash/FinalProject/script_bus.sh
+++ Linux/bash/FinalProject/script_bus.sh
@@ -8,9 +8,6 @@ TIME=$(date '+%Y-%m-%d %H:%M:%S')
 
 CURRENT=$(grep "N: Name=" /proc/bus/input/devices | cut -d'"' -f2 | sort) 
 
-
-
-
 if [ -f "$LIST_FILE" ]; then
     OLD=$(cat "$LIST_FILE")
 else
@@ -19,25 +16,40 @@ fi
 
 echo $CURRENT > $LIST_FILE
 
-
-printf "%-40s %-10s %-10s %-12s\n" "name" "vendor" "product" "handler"
-awk -v RS="" '{
-    name = $0; sub(/.*Name="/, "", name); sub(/".*/, "", name)
-    vendor = $0; sub(/.*Vendor=/, "", vendor); vendor = substr(vendor, 1, 4)
-    product = $0; sub(/.*Product=/, "", product); product = substr(product, 1, 4)
-    handler = $0; sub(/.*Handlers=/, "", handler); handler = substr(handler, 1, 10)
-    
-    printf "%-40s %-10s %-10s %-12s\n", name, vendor, product, handler
-}' /proc/bus/input/devices
-
-
-if [ -n "$OLD" ]; then
-    NEW=$(echo "$CURRENT" | grep -vxF "$OLD" || true)
+if [ -f "$BUS_LIST_FILE" ]; then
+    list_bus_old=$(tail -n +2 "$BUS_LIST_FILE")
 else
-    NEW="$CURRENT"
+    list_bus_old=""
 fi
 
-if [ -n "$NEW" ]; then
-    echo "[$TIME] Новые устройства:" >> "$LOG_FILE"
-    echo "$NEW" >> "$LOG_FILE"
+echo "bus!name!phys!sysfs!uniq!handlers!PROP!KEY!REL" > "$BUS_LIST_FILE"
+
+bus_info=""
+list_new=()
+while IFS= read -r line; do
+    if [[ "$line" == "" ]]; then
+        if [[ -n "$bus_info" ]]; then
+            echo "$bus_info" >> "$BUS_LIST_FILE"
+            if ! echo "$list_bus_old" | grep -q -F "$bus_info"; then
+                list_new+=( "$bus_info" )
+            fi
+            bus_info=""
+        fi
+    else
+        bus_info+="${line:3}!"
+    fi
+done < /proc/bus/input/devices
+
+if [[ -n "$bus_info" ]]; then
+    echo "$bus_info" >> "$BUS_LIST_FILE"
+    if ! echo "$list_bus_old" | grep -q -F "$bus_info"; then
+        list_new+=( "$bus_info" )
+    fi
+fi
+
+if [ ${#list_new[@]} -gt 0 ]; then
+    echo "[$TIME] New devices detected:" >> "$LOG_FILE"
+    for device in "${list_new[@]}"; do
+        echo "[$TIME] $device" >> "$LOG_FILE"
+    done
 fi
